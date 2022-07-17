72c254cdb967f1e6bbab6ace9a6557c7bebed222
Ritik Chelani <100259409+ritikchelani@users.noreply.github.com>
2022-07-17 11:53:55 +0530

    Update City.js

--- src/components/City.js
+++ src/components/City.js
@@ -1,177 +1,30 @@
 import React, { Component, useState } from "react";
-import "./../styles/App.css";
-import { StateDetails } from "./State";
+import Town from "./Town";
 
-// Do not alter the states const and values inside it.
-const states = [
-  {
-    name: "Madhya Pradesh",
-    cities: [
-      {
-        name: "Indore",
-        towns: [
-          {
-            name: "Mhow",
-          },
-          {
-            name: "Dewas",
-          },
-        ],
-      },
-      {
-        name: "Bhopal",
-        towns: [
-          {
-            name: "Manit",
-          },
-          {
-            name: "Berasia",
-          },
-        ],
-      },
-      {
-        name: "Gwalior",
-        towns: [
-          {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Jharkhand",
-    cities: [
-      {
-        name: "Dhanbad",
-        towns: [
-          {
-            name: "IIT(ISM) Dhanbad",
-          },
-          {
-            name: "Hirapur",
-          },
-        ],
-      },
-      {
-        name: "Wasseypur",
-        towns: [
-          {
-            name: "Sardar khan's",
-          },
-          {
-            name: "Faizal khan's",
-          },
-        ],
-      },
-      {
-        name: "Mirzapur",
-        towns: [
-          {
-            name: "Kaleen bhaiya's",
-          },
-          {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Assam",
-    cities: [
-      {
-        name: "Guwhati",
-        towns: [
-          {
-            name: "Amin",
-          },
-          {
-            name: "Jalah",
-          },
-        ],
-      },
-      {
-        name: "Jungle1",
-        towns: [
-          {
-            name: "Tiger found at IIT Guwahati",
-          },
-          {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
-      },
-      {
-        name: "Tezpur",
-        towns: [
-          {
-            name: "Dibrugarh",
-          },
-          {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Bihar",
-    cities: [
-      {
-        name: "Patna",
-        towns: [
-          {
-            name: "Sonpur",
-          },
-          {
-            name: "Maner",
-          },
-        ],
-      },
-      {
-        name: "Gaya",
-        towns: [
-          {
-            name: "Bakraur",
-          },
-          {
-            name: "Barachatti",
-          },
-        ],
-      },
-      {
-        name: "Darbhanga",
-        towns: [
-          {
-            name: "Singhwara",
-          },
-          {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
-];
+function City(props) {
+  const [showTowns, setTowns] = useState(false);
 
-function App() {
+  function show() {
+    return props.cityVar.towns.map((townName, townId) => {
+      return <Town townName={townName} townId={townId} key={townId} />;
+    });
+  }
+
   return (
-    <div id="main">
-      <div>
-        {states.map((stateName, stateId) => {
-          return (
-            <StateDetails
-              stateVar={stateName}
-              stateName={stateName.name}
-              stateId={stateId}
-              key={stateId}
-            />
-          );
-        })}
+    <>
+      <div
+        id={"city" + (props.cityId + 1)}
+        onClick={() => {
+          setTowns(!showTowns);
+        }}
+      >
+        {props.cityName}
       </div>
-    </div>
+      <div style={{ color: "green", paddingLeft: "20px" }}>
+        {showTowns ? show() : ""}
+      </div>
+    </>
   );
 }
 
-export default App;
+export default City;
